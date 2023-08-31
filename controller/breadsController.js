11a288fe1eea4ff76f93c2db0ccd0332c20fd093
akullyot <akullyot@gmail.com>
2023-08-30 19:35:29 -0500

    added a whitelisting to breads/:arrayIndex and started part 3

--- controller/breadsController.js
+++ controller/breadsController.js
@@ -8,13 +8,25 @@ breads.get('/', (request, response) =>
 {
   response.send(breadDataArray)
 });
-
+breads.get('/error', (request, response) => 
+{
+  response.send('You have entered an invalid bread array value');
+});
 //Dynamic Routes
 // SHOW
 breads.get('/:arrayIndex', (request, response) => 
 {
-    
-    response.send(breadDataArray[req.params.arrayIndex])
+    // whitelist only the indexes that exist in the breadDataArray
+    const breadDataArrayIndexes = Array.from({ length : breadDataArray.length}, (value, index) => index);
+    if (breadDataArrayIndexes.includes(parseInt(request.params.arrayIndex)))   
+    {
+        response.send(breadDataArray[request.params.arrayIndex]);
+    }
+    else
+    {
+        //redirect to the error page 
+        response.redirect('/breads/error');
+    }
 });
   
 
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app     = express();
 
+//Initialize the middleware
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+
 
 //Environment Variables
 const PORT = process.env.PORT;
@@ -17,6 +25,15 @@ app.get('/', (request,response) => {
 //Dynamic routes
 //Breads Controller
 app.use('/breads', require('./controller/breadsController.js'));
+
+//Catch all route
+app.get('*', (request,response) =>
+{
+    //chain together the sent HTML and the HTTP status
+    response.status(404).send('<h1> 404 Page not Found </h1>');
+
+});
+
 
 
 //Create ROUTES)
