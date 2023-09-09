4dbac42e965ea605e28f2d63e8b5abcf0a58ef0d
akullyot <akullyot@gmail.com>
2023-09-09 12:14:53 -0500

    changed nodemon updating to include more files

--- views/showBreadInfo.jsx
+++ views/showBreadInfo.jsx
@@ -19,7 +19,6 @@ function Show ({bread, index}) {
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
             <input type='submit' value="DELETE"/>
         </form>
-
         <li><a href="/breads">Go home</a></li>
         </Default>
     )
